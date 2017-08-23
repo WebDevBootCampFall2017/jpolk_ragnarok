61fe6c189a09d5ba1df0e83b98da4b49fa4dc12c
James Polk <jepolk@my.waketech.edu>
2017-08-23 07:52:05 -0400

    commit before pull

--- Ragnarok/Program.cs
+++ Ragnarok/Program.cs
@@ -38,9 +38,9 @@ namespace Ragnarok
 			System.Threading.Thread.Sleep(2000);
 			Console.WriteLine("	|___|  |_|__| |__|_______|_|  |__|__| |__|___|  |_|_______|___| |_|");
 			Console.ForegroundColor = ConsoleColor.Gray;
-			System.Threading.Thread.Sleep(1500);
+			System.Threading.Thread.Sleep(5000);
 			Console.WriteLine("\n\nThe promised end is upon you\nSummon the courage to fight for your possibility of a future"); //Tagline
-			System.Threading.Thread.Sleep(1500);
+			System.Threading.Thread.Sleep(5000);
 			Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write("\nDo you need instuctions? Type 1 (yes) or 0 (no), then press enter:"); //instruction question
 			Console.ForegroundColor = ConsoleColor.Gray;
@@ -56,7 +56,7 @@ namespace Ragnarok
 									"Engage is a desperate turn based battle to rid the world of\n" +
 									"NIDHOGG: The source of decay and blight upon this land\n" +
 									"Nidhogg is the dragon that threatens humanity as the prophecy demands\n");
-					System.Threading.Thread.Sleep(4000);
+					System.Threading.Thread.Sleep(6000);
 					Console.Write("------------------------\n" +
 									"BERSERKER's main weapon is the sword\n" +
 									"as Berserker uses the ATTACK command, it will build Rage and give access to\n" +
@@ -65,13 +65,13 @@ namespace Ragnarok
 									"BLOODLUST: player gains HP 2.5x the damage done to the target\n" +
 									"CHAOS BURST: high attack damage\n" +
 									"------------------------\n");
-					System.Threading.Thread.Sleep(4000);
+					System.Threading.Thread.Sleep(6000);
 					Console.Write("WIZARD's main weapon is a Magic Staff\n" +
 									"while the Wizard does not do much damage with the ATTACK command\n" +
 									"Wizard does have access to damage dealing spells" +
 									"with the MAGIC command at the cost of MP\n" +
                                     "------------------------\n");
-                    System.Threading.Thread.Sleep(4000);
+                    System.Threading.Thread.Sleep(6000);
                     Console.Write("MONK's main weapon are martial arts\n" +
                                     "Monk has a special command named HARNESS\n" +
                                     "by using harness you can build focus that will give you access" +
@@ -81,14 +81,17 @@ namespace Ragnarok
 									"AURA BOLT: high attack damage\n" +
 									"Monk also gains a small amount of focus with the ATTACK command\n" +
                                     "------------------------\n");
-					System.Threading.Thread.Sleep(4000);
+					System.Threading.Thread.Sleep(6000);
 					Console.Write("Every character has access to 5 Potions and 1 Hi-Potion\n" +
 									"Potions give 5,000 HP, Hi-Potion gives 10,000\n" +
 									"Wizard is given 2 Ethers to gain 300 MP on use\n" +
 									"You may enter an invalid selection in any of the battle submenus\n" +
 									"to take you back to the main battle menu\n" +
 									"------------------------\n\n");
-					System.Threading.Thread.Sleep(5000);
+					System.Threading.Thread.Sleep(3000);
+					Console.ForegroundColor = ConsoleColor.Yellow;
+					Console.Write("Press Enter to continue:");
+					Console.ReadKey();
 					Console.ForegroundColor = ConsoleColor.White;
 					Console.Write("STEEL YOUR NERVE and CHOOSE YOUR PATH\n");
 					Console.ForegroundColor = ConsoleColor.Yellow;
@@ -707,7 +710,7 @@ namespace Ragnarok
 								Console.Write("\n\nNidhogg retaliates with a fericous blow\n" +
 													"You suffer {0:N0} HP worth of damage!\n", player_turn_dmg);
 								Console.ForegroundColor = ConsoleColor.Yellow;
-								Console.WriteLine("\nNIDHOGG TURN END: Press enter\n");
+								Console.Write("\nNIDHOGG TURN END: Press enter\n");
 								Console.ForegroundColor = ConsoleColor.Gray;
 								Console.ReadKey(player_turn = true);
 							}
@@ -722,7 +725,7 @@ namespace Ragnarok
 								Console.Write("\n\nNidhogg retaliates with a fericous blow\n" +
 													"You suffer {0:N0} HP worth of damage!\n", player_turn_dmg);
 								Console.ForegroundColor = ConsoleColor.Yellow;
-								Console.WriteLine("\nNIDHOGG TURN END: Press enter");
+								Console.Write("\nNIDHOGG TURN END: Press enter\n");
 								Console.ForegroundColor = ConsoleColor.Gray;
 								Console.ReadKey(player_turn = true);
 							}
