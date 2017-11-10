dfb6ad91d9f1cb402c870c994a896fbd5f894f5e
James Polk <jepolk@my.waketech.edu>
2017-11-10 16:51:16 -0500

    enhanced battle canvas

--- jsRagnarok/battleHorizon.js
+++ jsRagnarok/battleHorizon.js
@@ -8,15 +8,14 @@ var canHeight = canvas.height
 var background = function()
 {
 	ct.beginPath();
-	ct.strokeStyle="#FFFFFF"
-	ct.lineWidth = 6;
-	ct.lineCap = "round";
-	ct.moveTo(0, canHeight * .3);
-	ct.bezierCurveTo(canWidth * .30, canHeight * .267, canWidth * .70, canHeight * .267, canWidth, canHeight * .3);
+	ct.strokeStyle = "#b54dff";
+	ct.moveTo(canWidth * .512, 0);
+	ct.lineTo(canWidth * .544, canHeight * .034);
 	ct.stroke();
 	ct.closePath();
 	ct.beginPath();
 	ct.lineWidth = 2.9;
+	ct.strokeStyle="#FFFFFF"
 	ct.moveTo(0, canHeight * .25);
 	ct.lineTo(canWidth * .10, canHeight * .22);
 	ct.lineTo(canWidth * .15, canHeight * .24);
@@ -25,19 +24,19 @@ var background = function()
 	ct.lineTo(canWidth * .56, canHeight * .02);
 	ct.lineTo(canWidth * .59, canHeight * .02);
 	ct.lineTo(canWidth * .60, canHeight * .01);
+	ct.stroke();
+	ct.closePath();
+	ct.beginPath();
+	ct.strokeStyle = "#9f1aff";
+	ct.moveTo(canWidth * .60, canHeight * .01);
 	ct.lineTo(canWidth * .73, canHeight * .19);
 	ct.lineTo(canWidth * .745, canHeight * .19);
 	ct.lineTo(canWidth * .792, canHeight * .25);
 	ct.stroke();
 	ct.closePath();
 	ct.beginPath();
-	ct.lineWidth = 4;
-	ct.moveTo(canWidth * .78, canHeight * .282);
-	ct.lineTo(canWidth * .89, 0);
-	ct.stroke();
-	ct.closePath();
-	ct.beginPath();
 	ct.lineWidth = .4;
+	ct.strokeStyle = "#FFFFFF";
 	ct.moveTo(0, canHeight * .105);
 	ct.lineTo(canWidth * .14, canHeight * .07);
 	ct.lineTo(canWidth * .208, canHeight * .117);
@@ -52,15 +51,33 @@ var background = function()
 	ct.stroke();
 	ct.closePath();
 	ct.beginPath();
-	ct.moveTo(canWidth * .512, 0);
-	ct.lineTo(canWidth * .544, canHeight * .034);
-	ct.stroke();
-	ct.closePath();
-	ct.beginPath();
 	ct.lineWidth = .4;
+	ct.strokeStyle = "#ca80ff";
 	ct.moveTo(canWidth * .716, canHeight * .17);
 	ct.lineTo(canWidth * .76, canHeight * .14);
 	ct.lineTo(canWidth * .813, canHeight * .2);
+	ct.stroke();
+	ct.closePath();
+	ct.beginPath();
+	ct.lineWidth = 4;
+	ct.strokeStyle = "#590099";
+	ct.moveTo(canWidth * .78, canHeight * .282);
+	ct.lineTo(canWidth * .89, 0);
+	ct.stroke();
+	ct.closePath();
+	ct.beginPath();
+	var moonLight = ct.createLinearGradient (0, 0, canWidth, 0);
+	moonLight.addColorStop("0.10", "#FFFFFF");
+	moonLight.addColorStop("0.52", "#590099");
+	moonLight.addColorStop("0.71", "#9f1aff");
+	moonLight.addColorStop("0.88", "#590099");
+	moonLight.addColorStop("1", "#FFFFFF");
+	ct.strokeStyle = moonLight;
+	ct.lineWidth = 6;
+	ct.lineCap = "round";
+	ct.moveTo(0, canHeight * .3);
+	ct.bezierCurveTo(canWidth * .30, canHeight * .267, canWidth * .70, canHeight * .267, canWidth, canHeight * .3);
+	ct.fill();
 	ct.stroke();
 	ct.closePath();
 }
@@ -72,10 +89,11 @@ background();
 var moon = function()
 {
 	ct.beginPath();
-	var moonGrad = ct.createRadialGradient(canWidth * .788, canHeight * .045, 3, canWidth * .79, canHeight * .052, 35);
-	moonGrad.addColorStop(0, "indigo");
+	var moonGrad = ct.createRadialGradient(canWidth * .791, canHeight * .04, 3, canWidth * .79, canHeight * .052, 42);
+	moonGrad.addColorStop(0, "#3b0066");
 	moonGrad.addColorStop(0.2, "white");
-	moonGrad.addColorStop(0.6, "indigo");
+	moonGrad.addColorStop(0.6, "#590099");
+	moonGrad.addColorStop(0.8, "#1e0033");
 	moonGrad.addColorStop(1, "black");
 	ct.fillStyle = moonGrad;
 	ct.fillRect(canWidth * .69, 0, canWidth * .145, canHeight * .11);
